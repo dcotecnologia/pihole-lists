8d56385f754ecb975d54978144fef4a23ef6cbd5
Danilo Carolino <danilogcarolino@gmail.com>
2026-03-21 18:11:14 -0300

    feat: add nolovia source for telemetry hosts

--- src/sources.py
+++ src/sources.py
@@ -5,4 +5,5 @@ SOURCES = {
     "someonewhocares": "https://someonewhocares.org/hosts/hosts",
     "yoyo": "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0",
     "adaway": "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt",
+    "nolovia": "https://raw.githubusercontent.com/parseword/nolovia/master/skel/hosts-windows-telemetry.txt",
 }
